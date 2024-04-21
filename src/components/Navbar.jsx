d5c13ad0e4cf6085e144d776d8c7059224722873
godpower13 <56938069+godpower13@users.noreply.github.com>
2024-04-21 18:38:27 +0300

    More Changes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import {} from "react-icons";
+const Navbar = () => {
+  return (
+    <div>
+      <div className="bg-black h-[100px]"></div>
+    </div>
+  );
+};
+
+export default Navbar;
